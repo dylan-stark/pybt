1231068a81dfb228e699ae8e3571dade3579595e
Dylan Stark <dylan.stark@gmail.com>
2018-02-21 18:26:23 -0600

    Moves to use kwargs.

--- examples/imdb.py
+++ examples/imdb.py
@@ -52,7 +52,8 @@ model.compile(optimizer=RMSprop(lr=0.001),
 
 # Create a population with this model and train for 10 steps
 pop = Population(models=model)
-model = pop.train(num_steps = 4, x=partial_x_train, y=partial_y_train)
+model = pop.train(num_steps = 4,
+    step_args = {'x': partial_x_train, 'y': partial_y_train})
 
 test_loss, test_acc = model.evaluate(x_test, y_test)
 
--- pybt/population.py
+++ pybt/population.py
@@ -8,27 +8,26 @@ class Population:
 
         self._models = models
 
-    def train(self, num_steps, x, y):
+    def train(self, num_steps, step_args):
         """Train a population for some number of steps."""
         if num_steps <= 0:
             raise ValueError('num_steps must be positive integer')
 
-        initial_epoch = 0
+        step_args['initial_epoch'] = 0
         epochs_per_step = 1
 
         member = self._sample_from_population()
         for _ in range(num_steps):
-            self._step(member, x, y, epochs=initial_epoch+epochs_per_step,
-                initial_epoch=initial_epoch)
-
-            initial_epoch += epochs_per_step
+            step_args['epochs'] = step_args['initial_epoch'] + epochs_per_step
+            self._step(member, **step_args)
+            step_args['initial_epoch'] += epochs_per_step
 
         return self._models[0]
 
     def _sample_from_population(self):
         return np.random.choice(self._models)
 
-    def _step(self, member, x, y, epochs, initial_epoch):
+    def _step(self, member, **step_args):
         model = member
-        model.fit(x=x, y=y, epochs=epochs, initial_epoch=initial_epoch)
+        model.fit(**step_args)
 
