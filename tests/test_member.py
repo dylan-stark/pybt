e44c018a243545d9954a4771b7542eda75a9d1d3
Dylan Stark <dylan.stark@gmail.com>
2018-02-23 05:34:17 -0600

    Adds test for history copy.

--- tests/test_member.py
+++ tests/test_member.py
@@ -1,3 +1,5 @@
+from copy import copy
+
 import pytest
 
 from pybt.member import Member
@@ -32,4 +34,23 @@ def test_step_history(simple_mnist_model, mnist_sample):
     m.step()
     assert len(m._histories) == 2
     assert len(m._histories[1]['loss']) == 2 # epochs per step
+
+def test_copy_history(simple_mnist_model, mnist_sample):
+    images, labels = mnist_sample
+    m = Member(simple_mnist_model,
+            step_args={'x': images, 'y': labels},
+            eval_args={'x': images, 'y': labels})
+
+    m.step()
+    m.step()
+    assert len(m._histories) == 2
+    assert len(m._histories[1]['loss']) == 2 # epochs per step
+
+    m2 = copy(m)
+    assert len(m2._histories) == 2
+    assert len(m2._histories[1]['loss']) == 2 # epochs per step
+    assert m._histories == m2._histories
+    m2.step()
+    assert m._histories != m2._histories
+    assert m._histories == m2._histories[:2]
 
