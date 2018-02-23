ea570da2b65da68fe0b13625063dd1424081f0d7
Dylan Stark <dylan.stark@gmail.com>
2018-02-23 04:57:00 -0600

    Collections and copies histories.

--- pybt/member.py
+++ pybt/member.py
@@ -1,31 +1,36 @@
-from copy import copy
+from copy import copy, deepcopy
 
 class Member:
     def __init__(self, model, step_args, eval_args):
         self._model = model
+        self._histories = []
         self._fit_args = copy(step_args)
         self._evaluate_args = copy(eval_args)
 
         self._fit_args['initial_epoch'] = 0
         self._fit_args['epochs'] = 0
-        self._epochs_per_step = 1
+        self._epochs_per_step = 2
 
     def __copy__(self):
         m = Member(self._model, self._fit_args, self._evaluate_args)
+        m._histories = deepcopy(self._histories)
         m._fit_args['initial_epoch'] = copy(self._fit_args['initial_epoch'])
         m._fit_args['epochs'] = copy(self._fit_args['epochs'])
 
         return m
 
     def __str__(self):
-        s = '\t{} (t={})'.format(self._model, self._fit_args['initial_epoch'])
+        s = '\t{} (t={})\n'.format(self._model, self._fit_args['initial_epoch'])
+        for h in self._histories:
+            s += '\t\tacc: {}\n'.format(h['acc'])
 
         return s
 
     def step(self):
         self._fit_args['epochs'] = \
             self._fit_args['initial_epoch'] + self._epochs_per_step
-        self._model.fit(**self._fit_args)
+        history = self._model.fit(**self._fit_args)
+        self._histories.append(copy(history.history))
         self._fit_args['initial_epoch'] += self._epochs_per_step
 
     def eval(self):
