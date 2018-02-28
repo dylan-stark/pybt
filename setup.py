077fb206b5ff0177d5792ac9f139ee17da56d165
Dylan Stark <dylan.stark@gmail.com>
2018-02-28 16:45:31 -0600

    Bumps version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='pybt',
-      version='0.0.1',
+      version='0.1.0',
       description='Simple Populatin Based Training',
       long_description=readme(),
       url='http://github.com/dylan-stark/pybt',
